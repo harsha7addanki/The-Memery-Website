40ab7343d6590d8cc6e01a172197cfd4649f2533
Harsha Addanki <harsha7addanki@gmail.com>
2022-11-12 09:11:25 -0500

    modified:   .gitignore 	modified:   next.config.js 	modified:   package.json 	new file:   pages/_document.js 	modified:   public/favicon.ico 	new file:   public/icon-192x192.png 	new file:   public/icon-256x256.png 	new file:   public/icon-384x384.png 	new file:   public/icon-512x512.png 	new file:   public/manifest.webmanifest 	modified:   yarn.lock

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
-const withWorkbox = require('next-with-workbox');
+const withPWA = require('next-pwa')(
+  {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable:process.env.NODE_ENV === 'development'
+  }  
+);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  workbox:{
-    
-  },
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = withWorkbox(nextConfig)
+module.exports = withPWA(nextConfig)
